87e85d4a7df2da1761637a8e40ad2d1818e2b2a0
Carlos Bezerra <carlosfilipe.lb@gmail.com>
2022-01-11 18:30:02 -0300

    making idea's validation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
 
+const dialog = require("./dialog.json")
+
 function App() {
+
+  const audioURL = "https://www.buzzsprout.com/1192367/4381745-001-coffee-a-short-english-conversation-by-dialogue-frog.mp3"
+
+
+  function renderText() {
+
+    const renderedTranscript = [];
+
+    dialog.transcript.forEach(saying => {
+
+      const code = <div>
+        <p>{saying.person}</p>
+        {
+          saying.phrases.map(phrase => {
+            return <p onClick={ () => {
+              const audio = document.getElementById('audio');
+              audio.currentTime = phrase.time;
+              audio.play();
+            }
+            }>{phrase.text}</p>
+          })
+
+        }
+
+      </div>
+
+      renderedTranscript.push(code)
+
+
+    });
+
+
+    return renderedTranscript;
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <audio id="audio" controls autoplay >
+        <source src={audioURL} type="audio/mpeg"/>
+      Your browser does not support the audio element.
+      </audio>
+
+
+      {renderText()}
+
     </div>
   );
 }
